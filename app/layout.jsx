46a865fb0d4b11fe75b6fa13a3393ab75612bdde
Nguyen Phi Nam <nguyenphinam2k2@gmail.com>
2023-06-07 01:33:51 +0700

    next auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,27 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover and share AI promp"
-}
+  title: "Promptopia",
+  description: "Discover and share AI promp",
+};
 const RootLayout = ({ children }) => {
-    return (
-        <html lang="en">
-            <div className='main'>
-                <div className='gradient' />
-            </div>
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-            <main className='app'>
-                {children}
-            </main>
-        </html>
-    )
-}
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
 export default RootLayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,25 @@
-import Feed from "@components/Feed"
+"use client";
+
+import Feed from "@components/Feed";
+import Image from "next/image";
 
 const Home = () => {
   return (
-    <section className='w-full flex-center flex-col'>
-        <h1 className='head_text text-center'>
-            Discover & Share
-            <br className='max-md:hidden' />
-            <span className='orange_gradient text-center'> 
-                AI-Powred Prompts 
-            </span>
-        </h1>
-            <p className='desc text-center'>
-                Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts.
-            </p>
-   </section>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powred Prompts</span>
+      </h1>
 
-export default Home
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts.
+      </p>
+
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
